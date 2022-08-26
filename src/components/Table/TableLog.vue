d8cb82818a28991627f983d97a4784a1aa9604b5
Munkh-Erdene Zorigbold <zemunkh@gmail.com>
2022-08-26 18:40:50 +0800

    Datatable is updated

--- src/components/Table/TableLog.vue
+++ src/components/Table/TableLog.vue
@@ -3,14 +3,16 @@ import Datepicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 import { onMounted, ref } from "vue";
 // import { useMainStore } from "@/stores/main";
-import { mdiEye, mdiArrowBottomLeft } from "@mdi/js";
+import { mdiArrowBottomLeft } from "@mdi/js";
 import CardBoxModal from "@/components/CardBoxModal.vue";
 // import TableCheckboxCell from "@/components/TableCheckboxCell.vue";
 import PillTag from "@/components/PillTag.vue";
-import BaseButtons from "@/components/BaseButtons.vue";
 import BaseButton from "@/components/BaseButton.vue";
+import FormControl from "@/components/FormControl.vue";
+import FormField from "@/components/FormField.vue";
 import moment from "moment";
 import CardBox from "../CardBox.vue";
+import { FilterMatchMode, FilterOperator } from "primevue/api";
 // import UserAvatar from "@/components/UserAvatar.vue";
 
 defineProps({
@@ -18,13 +20,40 @@ defineProps({
 });
 const today = new Date();
 const isModalActive = ref(false);
-const checkedRows = ref([]);
 
 const startDate = ref(null);
 const endDate = ref(today);
 
 const logData = ref(null);
 const error = ref(null);
+
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  stockName: {
+    operator: FilterOperator.AND,
+    constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+  },
+  created_at: {
+    operator: FilterOperator.AND,
+    constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
+  },
+  machine: {
+    operator: FilterOperator.OR,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+  shift: {
+    operator: FilterOperator.OR,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+  category: {
+    operator: FilterOperator.OR,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+  action: {
+    operator: FilterOperator.OR,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+});
 
 onMounted(async () => {
   startDate.value = new Date(today.getTime() - 1 * 24 * 60 * 60 * 1000);
@@ -42,7 +71,7 @@ const fetchDataByRange = () => {
     .then((res) => res.json())
     .then((json) => (logData.value = json))
     .catch((err) => (error.value = err));
-}
+};
 
 const actionColor = (action) => {
   if (action === "create") {
@@ -54,29 +83,6 @@ const actionColor = (action) => {
   }
   return "info";
 };
-
-// const remove = (arr, cb) => {
-//   const newArr = [];
-
-//   arr.forEach((item) => {
-//     if (!cb(item)) {
-//       newArr.push(item);
-//     }
-//   });
-
-//   return newArr;
-// };
-
-// const checked = (isChecked, client) => {
-//   if (isChecked) {
-//     checkedRows.value.push(client);
-//   } else {
-//     checkedRows.value = remove(
-//       checkedRows.value,
-//       (row) => row.id === client.id
-//     );
-//   }
-// };
 </script>
 
 <template>
@@ -84,16 +90,6 @@ const actionColor = (action) => {
     <p>Lorem ipsum dolor sit amet <b>adipiscing elit</b></p>
     <p>This is sample modal</p>
   </CardBoxModal>
-
-  <div v-if="checkedRows.length" class="p-3 bg-gray-100/50 dark:bg-slate-800">
-    <span
-      v-for="checkedRow in checkedRows"
-      :key="checkedRow.id"
-      class="inline-block px-2 py-1 rounded-sm mr-2 text-sm bg-gray-100 dark:bg-slate-700"
-    >
-      {{ checkedRow.name }}
-    </span>
-  </div>
 
   <CardBox>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
@@ -119,66 +115,62 @@ const actionColor = (action) => {
     </div>
   </CardBox>
 
-  <CardBox has-table>
-    <table>
-      <thead>
-        <tr>
-          <th v-if="checkable" />
-          <th>Action</th>
-          <th>Stock Code</th>
-          <th>Stock Name</th>
-          <th>Machine</th>
-          <th>Shift</th>
-          <th>Created At</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="log in logData" :key="log.id">
-          <!-- <TableCheckboxCell
-            v-if="checkable"
-            @checked="checked($event, log)"
-          /> -->
-          <td data-label="Action">
-            <PillTag
-              :color="actionColor(log.action)"
-              :label="log.action"
-              small
+  <CardBox>
+    <DataTable
+      v-model:filters="filters"
+      :value="logData"
+      responsive-layout="scroll"
+      :global-filter-fields="[
+        'stockName',
+        'action',
+        'machine',
+        'shift',
+        'category',
+        'created_at',
+      ]"
+    >
+      <template #header>
+        <div class="flex justify-content-center align-items-center">
+          <FormField label="Search" help="Below data table will be filtered">
+            <FormControl
+              v-model="filters['global'].value"
+              type="text"
+              placeholder="Search text"
             />
-          </td>
-          <td data-label="StockCode">
-            {{ log.stockCode }}
-          </td>
-          <td data-label="StockName">
-            {{ log.stockName }}
-          </td>
-          <td data-label="Machine">
-            {{ log.machine }}
-          </td>
-          <td data-label="Shift">
-            {{ log.shift }}
-          </td>
-          <td data-label="Created" class="lg:w-1 whitespace-nowrap">
-            <small
-              class="text-gray-500 dark:text-slate-400"
-              :title="moment(new Date(log.created_at)).format('LLLL')"
-              >{{
-                moment(new Date(log.created_at)).format("MM/DD/YYYY, h:mm a")
-              }}</small
-            >
-          </td>
-          <td class="before:hidden lg:w-1 whitespace-nowrap">
-            <BaseButtons type="justify-start lg:justify-end" no-wrap>
-              <BaseButton
-                color="info"
-                :icon="mdiEye"
-                small
-                @click="isModalActive = true"
-              />
-            </BaseButtons>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </FormField>
+        </div>
+      </template>
+      <Column field="action" header="Action" :sortable="true">
+        <template #body="{ data }">
+          <PillTag
+            :color="actionColor(data.action)"
+            :label="data.action"
+            small
+          />
+        </template>
+      </Column>
+      <Column field="stockCode" header="Stock Code" :sortable="true"></Column>
+      <Column field="stockName" header="Stock Name" :sortable="true"></Column>
+      <Column field="machine" header="Machine" :sortable="true"></Column>
+      <Column field="shift" header="Shift" :sortable="true"></Column>
+      <Column field="totalQty" header="Total qty" :sortable="true"></Column>
+      <Column field="category" header="Category" :sortable="true"></Column>
+      <Column field="stockGroup" header="Group" :sortable="true"></Column>
+      <Column field="class" header="Class" :sortable="true"></Column>
+      <Column field="purchasePrice" header="Price" :sortable="true"></Column>
+      <Column field="uom" header="UOM" :sortable="true"></Column>
+      <Column field="weight" header="Weight" :sortable="true"></Column>
+      <Column field="created_at" header="Created At" :sortable="true">
+        <template #body="{ data }">
+          <small
+            class="text-gray-500 dark:text-slate-400"
+            :title="moment(new Date(data.created_at)).format('LLLL')"
+            >{{
+              moment(new Date(data.created_at)).format("MM/DD/YYYY, h:mm a")
+            }}</small
+          >
+        </template>
+      </Column>
+    </DataTable>
   </CardBox>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,17 +3,35 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from 'primevue/config';
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup";
+import InputText from "primevue/inputtext";
+import Row from "primevue/row";
 import { useMainStore } from "@/stores/main.js";
 import { useStyleStore } from "@/stores/style.js";
 import { darkModeKey, styleKey } from "@/config.js";
-
+import 'primeicons/primeicons.css';
 import "./css/main.css";
 
 /* Init Pinia */
 const pinia = createPinia();
 
 /* Create Vue app */
-createApp(App).use(router).use(pinia).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.use(PrimeVue);
+app.component("DataTable", DataTable);
+// eslint-disable-next-line vue/multi-word-component-names
+app.component("Column", Column);
+app.component("ColumnGroup", ColumnGroup);
+// eslint-disable-next-line vue/multi-word-component-names
+app.component("Row", Row);
+app.component("InputText", InputText);
+
+app.mount("#app");
 
 /* Init Pinia stores */
 const mainStore = useMainStore(pinia);
